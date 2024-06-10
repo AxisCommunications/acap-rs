0a5bc56771d2098d4716db97b5ec7e517c2fbbdf
AP Ljungquist <anderspl@axis.com>
2024-06-10 09:12:13 +0200

    chore: Group and sort imports (#23)
    This allows me to pay no attention to how I or my IDE adds imports adds
    imports.
    
    Don't group and sort imports automatically because this functionality
    is unstable.

--- apps/licensekey_handler/src/main.rs
+++ apps/licensekey_handler/src/main.rs
@@ -1,8 +1,11 @@
 //! A simple example application demonstrating how the licensekey crate may be used
 
+use std::{
+    ffi::{CStr, CString},
+    os::unix::ffi::OsStrExt,
+};
+
 use log::{info, warn};
-use std::ffi::{CStr, CString};
-use std::os::unix::ffi::OsStrExt;
 
 const APP_ID: i32 = 0;
 const MAJOR_VERSION: i32 = 1;
