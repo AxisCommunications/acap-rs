6a7cd437dbe1b0b92feeebd6afb3d209024804a5
AP Ljungquist <ap@ljungquist.eu>
2024-06-15 08:54:06 +0200

    fix(embedded_web_page): Put web page in `html/`
    The `acap-build` tool establishes the convention that the `html/`
    and `lib/` directories in the current working directory are special and
    always included. This seems convenient, so going forward the tools and
    examples in this project will follow that convention.

--- apps/embedded_web_page/src/main.rs
+++ apps/embedded_web_page/src/main.rs
@@ -3,3 +3,21 @@
 //! All applications require a program, but for this example it doesn't need to do anything,
 //! hence the empty main function.
 fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use std::{env, path::PathBuf};
+
+    fn html_dir() -> PathBuf {
+        env::current_exe().unwrap().parent().unwrap().join("html")
+    }
+    #[test]
+    fn html_files_are_installed() {
+        assert!(html_dir().join("index.html").is_file())
+    }
+
+    #[test]
+    fn html_nested_files_are_installed() {
+        assert!(html_dir().join("css").join("main.css").is_file())
+    }
+}
