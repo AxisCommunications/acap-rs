536d7cae0139044a8d54f38089c28c8fa956e957
Anders-Petter Ljungquist <anderspl@axis.com>
2024-06-18 08:23:13 +0200

    Use purpose built app for testing

--- apps/hello_world/src/main.rs
+++ apps/hello_world/src/main.rs
@@ -5,10 +5,8 @@
 //! 2. how to build and bundle them as an application.
 
 use log::info;
-use std::collections::HashMap;
 
 fn main() {
     app_logging::init_logger();
-    info!("args: {:?}", std::env::args().collect::<Vec<_>>());
-    info!("vars: {:#?}", std::env::vars().collect::<HashMap<_, _>>());
+    info!("Hello World!");
 }
---
+++ apps/inspect_env/src/main.rs
@@ -0,0 +1,46 @@
+//! A simple app that inspects the environment it runs in
+
+use log::info;
+use std::{collections::HashMap, env};
+
+fn main() {
+    app_logging::init_logger();
+    info!("args: {:?}", env::args().collect::<Vec<_>>());
+    info!("vars: {:#?}", env::vars().collect::<HashMap<_, _>>());
+    info!("current_dir: {:?}", env::current_dir());
+    info!("current_exe: {:?}", env::current_exe());
+    info!("temp_dir: {:?}", env::temp_dir());
+}
+
+#[cfg(not(target_arch = "x86_64"))]
+#[cfg(test)]
+mod tests {
+    use std::env;
+    use std::path::PathBuf;
+
+    // None of these are officially guaranteed by the ACAP framework,
+    // but they seem to work in practice.
+
+    const PACKAGE_NAME: &str = "inspect_env";
+    fn package_dir() -> PathBuf {
+        PathBuf::from("/usr/local/packages").join(PACKAGE_NAME)
+    }
+
+    #[test]
+    fn selected_vars_are_set_as_expected() {
+        assert_eq!(env::var("G_SLICE").unwrap(), "always-malloc");
+    }
+
+    #[test]
+    fn current_dir_is_set_as_expected() {
+        assert_eq!(env::current_dir().unwrap(), package_dir())
+    }
+
+    #[test]
+    fn current_exe_is_set_as_expected() {
+        assert_eq!(
+            env::current_exe().unwrap(),
+            package_dir().join(PACKAGE_NAME)
+        )
+    }
+}
