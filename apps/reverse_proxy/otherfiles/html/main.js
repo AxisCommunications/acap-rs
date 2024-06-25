db61a7eb8b71b76238ea1d4690f035f45d5c8b53
Anders-Petter Ljungquist <anderspl@axis.com>
2024-06-25 09:36:09 +0200

    s: Add ws endpoint and web page
    The web page is meant to facilitate manual testing of the app.

---
+++ apps/reverse_proxy/otherfiles/html/main.js
@@ -0,0 +1,42 @@
+function update_auth() {
+    const url = '/local/reverse_proxy/api/admin/whoami';
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById('authentication').innerText = `Authenticated as ${data}`;
+        })
+        .catch(error => {
+            document.getElementById('authentication').innerText = 'Error: ' + error;
+        });
+}
+
+function update_time() {
+    const url =
+        (window.location.protocol === "https:" ? "wss://" : "ws://") +
+        window.location.host +
+        "/local/reverse_proxy/api/admin/ws";
+    const socket = new WebSocket(url);
+
+    socket.onmessage = function (event) {
+        const timestamp = event.data;
+        document.getElementById('timestamp').innerText = `The time is now ${timestamp}.`;
+    };
+
+    socket.onerror = function (error) {
+        document.getElementById('timestamp').innerText = 'WebSocket error: ' + error;
+    };
+
+    socket.onclose = function () {
+        document.getElementById('timestamp').innerText = 'WebSocket connection closed';
+    };
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    update_auth()
+    update_time()
+});
--- apps/reverse_proxy/src/main.rs
+++ apps/reverse_proxy/src/main.rs
@@ -1,31 +1,83 @@
 //! An example of how to run a webserver
 
+use axum::extract::ws::{Message, WebSocket};
+use axum::extract::WebSocketUpgrade;
+use axum::response::IntoResponse;
 use axum::{extract::Path, routing::get, Router};
-use log::debug;
-use serde::Deserialize;
+use futures::stream::{SplitSink, SplitStream};
+use futures::{SinkExt, StreamExt};
+use log::{debug, error, info, warn};
+use serde::{Deserialize, Serialize};
+use std::convert::Infallible;
+use std::time::{Duration, SystemTime};
+use tokio::time::sleep;
+
 use tower_http::trace::{DefaultMakeSpan, TraceLayer};
 
+const APP_NAME: &str = "reverse_proxy";
 
-const ACAP_NAME: &str = "reverse_proxy";
-
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 #[serde(rename_all = "snake_case")]
-enum Group {
+enum Scope {
     Admin,
     Operator,
     Viewer,
+    Anonymous,
 }
 
-async fn hello_authenticated_user(Path(group): Path<Group>) -> String {
-    format!("Hello {group:?}")
+async fn whoami(Path(scope): Path<Scope>) -> impl IntoResponse {
+    match scope {
+        Scope::Admin => "admin",
+        Scope::Operator => "operator",
+        Scope::Viewer => "viewer",
+        Scope::Anonymous => "anonymous",
+    }
+}
+
+async fn ws(ws: WebSocketUpgrade) -> impl IntoResponse {
+    ws.on_upgrade(|s| async {
+        let (sender, receiver) = s.split();
+        tokio::select!(
+            Err(e) = publish_time(sender) => error!("Closing websocket because {e:?}"),
+            _ = discard_inbound(receiver) => {},
+        );
+    })
+}
+
+async fn publish_time(
+    mut stream: SplitSink<WebSocket, Message>,
+) -> Result<Infallible, axum::Error> {
+    loop {
+        // Unwrap is OK `now` is always after `UNIX_EPOCH` on well-configured systems.
+        let now = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+        stream.send(Message::Text(format!("{now}"))).await?;
+        sleep(Duration::from_secs(1)).await
+    }
+}
+
+async fn discard_inbound(mut stream: SplitStream<WebSocket>) {
+    while let Some(msg) = stream.next().await {
+        match msg {
+            Ok(Message::Text(msg)) => warn!("Discarding inbound text {msg}"),
+            Ok(Message::Binary(_)) => warn!("Discarding inbound binary"),
+            Ok(Message::Ping(_)) => {}
+            Ok(Message::Pong(_)) => {}
+            Ok(Message::Close(msg)) => info!("Client is closing the connection {msg:?}"),
+            Err(e) => {
+                warn!("Failed to discard inbound because {e}");
+                break;
+            }
+        }
+    }
 }
 
 fn new_app() -> Router {
     Router::new()
-        .route(
-            &format!("/local/{ACAP_NAME}/api/:group"),
-            get(hello_authenticated_user),
-        )
+        .route(&format!("/local/{APP_NAME}/api/:scope/whoami"), get(whoami))
+        .route(&format!("/local/{APP_NAME}/api/:scope/ws"), get(ws))
         .layer(
             TraceLayer::new_for_http().make_span_with(DefaultMakeSpan::new().include_headers(true)),
         )
