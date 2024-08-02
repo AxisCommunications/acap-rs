a77f03024cd65182fd5520167ffc088fe9757582
Anders-Petter Ljungquist <anderspl@axis.com>
2024-08-02 14:24:19 +0200

    feat: Add bounding box wrapper and example

--- crates/axevent/src/lib.rs
+++ crates/axevent/src/lib.rs
@@ -1,7 +1,4 @@
 //! Bindings for the [Event API](https://axiscommunications.github.io/acap-documentation/docs/api/src/api/axevent/html/index.html).
-//!
-//! This crate provide two APIs with different goals:
-//! - [`ergo`] strives to enable all but the most exotic use cases in an easy and idiomatic way.
-//! - [`flex`] strives to facilitate transitioning from C.
+
 pub mod ergo;
 pub mod flex;
