f9e05066df35477f115a4636c44c1493baca5c57
Jonathan Green <Jsgjkjzi@gmail.com>
2024-10-31 07:38:56 -0400

    Start adding LarodDevice and Session - Stubbed out functions for Session (larodConnection) - Start implementing a few functions to list hand handle larodDevices

--- crates/larod/src/lib.rs
+++ crates/larod/src/lib.rs
@@ -17,11 +17,13 @@
 //! - [ ] [larodDisconnect](https://axiscommunications.github.io/acap-documentation/docs/api/src/api/larod/html/larod_8h.html#ab8f97b4b4d15798384ca25f32ca77bba)
 //!     indicates it may fail to "kill a session." What are the implications if it fails to kill a session? Can we clear the sessions?
 
-use core::slice;
+use core::{num, slice};
 use larod_sys::*;
 use std::{
-    ffi::{c_char, CStr, CString},
+    collections::HashMap,
+    ffi::{c_char, c_int, CStr, CString},
     ptr::{self, slice_from_raw_parts},
+    slice::from_raw_parts,
 };
 
 type Result<T> = std::result::Result<T, LarodError>;
@@ -43,15 +45,15 @@ macro_rules! try_func {
 // Now we will be able to write our own errors, defer to an underlying error
 // implementation, or do something in between.
 #[derive(Debug, Clone, Default)]
-struct LarodError {
-    msg: String,
-    code: LarodErrorCode,
+pub struct LarodError {
+    pub msg: String,
+    pub code: LarodErrorCode,
 }
 
-/// Convert from liblarod larodError to LarodError
-/// If larodError is not NULL, it must be dealocated by calling larodClearError
-impl From<*mut larodError> for LarodError {
-    fn from(mut e: *mut larodError) -> Self {
+impl LarodError {
+    /// Convert from liblarod larodError to LarodError
+    /// If larodError is not NULL, it must be dealocated by calling larodClearError
+    fn from(e: *mut larodError) -> Self {
         if e.is_null() {
             Self::default()
         } else {
@@ -63,9 +65,9 @@ impl From<*mut larodError> for LarodError {
                     .into()
             };
             let code: LarodErrorCode = le.code.into();
-            unsafe {
-                larodClearError(&mut e);
-            }
+            // unsafe {
+            //     larodClearError(&mut e);
+            // }
             Self { msg, code }
         }
     }
@@ -347,32 +349,207 @@ impl std::ops::Drop for LarodMap {
     }
 }
 
-pub struct LarodClientBuilder {}
+#[derive(Debug)]
+pub struct LarodDevice {
+    ptr: *const larodDevice,
+    name: String,
+    id: u32,
+}
 
-impl LarodClientBuilder {
-    pub fn build() -> Result<LarodClient> {
-        let mut connection: *mut larodConnection = ptr::null_mut();
-        let (success, e): (bool, LarodError) = unsafe { try_func!(larodConnect, &mut connection) };
+impl LarodDevice {
+    pub fn get_name(&self) -> &str {
+        &self.name
+    }
+
+    fn larod_get_name(pointer: *const larodDevice) -> Result<String> {
+        unsafe {
+            let (c_char_ptr, error) = try_func!(larodGetDeviceName, pointer);
+            if c_char_ptr.is_null() {
+                Err(error)
+            } else {
+                debug_assert!(
+                    matches!(error.code, LarodErrorCode::NONE),
+                    "larodGetDeviceName returned an object pointer AND returned an error!"
+                );
+                let c_name = CStr::from_ptr(c_char_ptr);
+                c_name
+                    .to_str()
+                    .map(|n| String::from(n))
+                    .map_err(|e| LarodError {
+                        msg: String::from("Returned string is not valid UTF-8"),
+                        code: LarodErrorCode::INVALID_TYPE,
+                    })
+            }
+        }
+    }
+
+    fn larod_get_instance(pointer: *const larodDevice) -> Result<u32> {
+        unsafe {
+            let mut instance: u32 = 0;
+            let (success, error) = try_func!(larodGetDeviceInstance, pointer, &mut instance);
+            if success {
+                debug_assert!(
+                    matches!(error.code, LarodErrorCode::NONE),
+                    "larodGetDeviceInstance returned success AND returned an error!"
+                );
+                Ok(instance)
+            } else {
+                Err(error)
+            }
+        }
+    }
+}
+
+impl TryFrom<*const larodDevice> for LarodDevice {
+    type Error = LarodError;
+    fn try_from(value: *const larodDevice) -> Result<Self> {
+        let name = LarodDevice::larod_get_name(value)?;
+        let id = LarodDevice::larod_get_instance(value)?;
+        Ok(Self {
+            ptr: value,
+            name,
+            id,
+        })
+    }
+}
+
+pub struct SessionBuilder {}
+
+impl SessionBuilder {
+    pub fn new() -> SessionBuilder {
+        SessionBuilder {}
+    }
+    pub fn build(&self) -> Result<Session> {
+        let mut conn: *mut larodConnection = ptr::null_mut();
+        let (success, e): (bool, LarodError) = unsafe { try_func!(larodConnect, &mut conn) };
         if success {
             debug_assert!(
                 matches!(e.code, LarodErrorCode::NONE),
                 "larodConnect indicated success AND returned an error!"
             );
-            Ok(LarodClient { connection })
+            Ok(Session {
+                conn,
+                model_map: HashMap::new(),
+                devices: Vec::new(),
+            })
         } else {
             Err(e)
         }
     }
 }
 
-pub struct LarodClient {
-    connection: *mut larodConnection,
+pub struct Session {
+    conn: *mut larodConnection,
+    model_map: HashMap<String, u64>,
+    devices: Vec<larodDevice>,
 }
 
-impl std::ops::Drop for LarodClient {
+// Using a session builder might not be necessary.
+// There's little to configure when starting a session.
+impl Session {
+    /// Constructs a new `Session`.
+    ///
+    /// # Panics
+    ///
+    /// Use `Session::builder()` if you wish to handle the failure as an `Error`
+    /// instead of panicking.
+    pub fn new() -> Session {
+        SessionBuilder::new().build().expect("Session::new()")
+    }
+    pub fn builder() -> SessionBuilder {
+        SessionBuilder::new()
+    }
+    pub fn disconnect(&mut self) -> Result<()> {
+        let (success, e): (bool, LarodError) =
+            unsafe { try_func!(larodDisconnect, &mut self.conn) };
+        if success {
+            debug_assert!(
+                matches!(e.code, LarodErrorCode::NONE),
+                "larodDisconnect indicated success AND returned an error!"
+            );
+            Ok(())
+        } else {
+            Err(e)
+        }
+    }
+    pub fn num_sessions() -> Result<()> {
+        Ok(())
+    }
+    pub fn device() -> Result<()> {
+        Ok(())
+    }
+    pub fn list_chips() -> Result<()> {
+        Ok(())
+    }
+    pub fn list_devices(&mut self) -> Result<Vec<LarodDevice>> {
+        let mut num_devices: usize = 0;
+        let (d, e) = unsafe {
+            let (dev_ptr, e) = try_func!(larodListDevices, self.conn, &mut num_devices);
+            if dev_ptr.is_null() {
+                return Err(e);
+            }
+            let raw_devices = unsafe { slice::from_raw_parts(dev_ptr, num_devices) };
+            let mut devices: Vec<LarodDevice> = Vec::with_capacity(num_devices);
+            for (idx, raw_device) in raw_devices.into_iter().enumerate() {
+                let device = LarodDevice::try_from(*raw_device);
+                match device {
+                    Ok(d) => devices.push(d),
+                    Err(conv_error) =>
+                    log::error!("Could not identify larodDevice {} of {} returned from larodListDevices.\n{}", idx, num_devices, conv_error.msg),
+                }
+            }
+            (devices, e)
+        };
+        Ok(d)
+    }
+
+    // Overloaded need to check that.
+    pub fn load_model(&mut self) -> Result<()> {
+        // let model_fd: c_int = 0;
+        // let (m, e) = unsafe {
+        //     try_func!(larodLoadModel, &mut self.conn, model_fd, )
+        // }
+        Ok(())
+    }
+    pub fn get_model() -> Result<()> {
+        Ok(())
+    }
+    pub fn get_models() -> Result<()> {
+        Ok(())
+    }
+    pub fn delete_model() -> Result<()> {
+        Ok(())
+    }
+    pub fn alloc_model_inputs() -> Result<()> {
+        Ok(())
+    }
+    pub fn alloc_model_outputs() -> Result<()> {
+        Ok(())
+    }
+    pub fn destroy_tensors() -> Result<()> {
+        Ok(())
+    }
+    pub fn track_tensor() -> Result<()> {
+        Ok(())
+    }
+    pub fn run_job() -> Result<()> {
+        Ok(())
+    }
+    pub fn run_inference() -> Result<()> {
+        Ok(())
+    }
+    pub fn chip_id() -> Result<()> {
+        Ok(())
+    }
+    pub fn chip_type() -> Result<()> {
+        Ok(())
+    }
+}
+
+impl std::ops::Drop for Session {
     fn drop(&mut self) {
         unsafe {
-            try_func!(larodDisconnect, &mut self.connection);
+            try_func!(larodDisconnect, &mut self.conn);
         }
     }
 }
