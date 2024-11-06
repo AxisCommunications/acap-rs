de2367be7a379c1e6ecb308c4eb454ab7a6c1f83
Jonathan Green <Jsgjkjzi@gmail.com>
2024-11-05 21:51:25 -0500

    Remove note that is no longer needed.

--- remote-test.sh
+++ remote-test.sh
@@ -6,5 +6,4 @@ if [ -n "${CARGO_TEST_CAMERA}" ]; then
     # echo $f
     ssh $CARGO_TEST_CAMERA "chmod +x /root/$f" 
     ssh $CARGO_TEST_CAMERA "/root/$f"
-    # ^ note: may need to change this line, see https://stackoverflow.com/q/9379400
 fi
