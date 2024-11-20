124a5a10ea8a7b17b10e797d00011a84c165a77a
AP Ljungquist <ap@ljungquist.eu>
2024-11-20 21:45:43 +0100

    no_std: >268K
    Some functionality was sacrificed.

--- apps/licensekey_handler/src/main.rs
+++ apps/licensekey_handler/src/main.rs
@@ -1,12 +1,9 @@
-#![forbid(unsafe_code)]
+#![no_std]
 //! A simple example application demonstrating how the licensekey crate may be used
 
-use std::{
-    ffi::{CStr, CString},
-    os::unix::ffi::OsStrExt,
-};
-
-use log::{info, warn};
+extern crate alloc;
+use core::ffi::CStr;
+use libc::sleep;
 
 const APP_ID: i32 = 0;
 const MAJOR_VERSION: i32 = 1;
@@ -14,23 +11,16 @@ const MINOR_VERSION: i32 = 0;
 
 fn check_license_status(app_name: &CStr) {
     match licensekey::verify(app_name, APP_ID, MAJOR_VERSION, MINOR_VERSION) {
-        Ok(()) => info!("License key is valid"),
-        Err(e) => warn!("License key is invalid because {e}"),
+        Ok(()) => {}
+        Err(_) => {}
     }
 }
 
 fn main() {
-    acap_logging::init_logger();
-    let app_name = CString::new(
-        std::env::current_exe()
-            .unwrap()
-            .file_name()
-            .unwrap()
-            .as_bytes(),
-    )
-    .unwrap();
     loop {
-        check_license_status(&app_name);
-        std::thread::sleep(std::time::Duration::from_secs(300));
+        check_license_status(c"licensekey_handler");
+        unsafe {
+            sleep(300);
+        }
     }
 }
