774322f64f4fa584ad787405aabccba4d6a66e4b
Jonathan Green <Jsgjkjzi@gmail.com>
2024-11-20 09:50:22 -0500

    Add configuration functions to StreamBuilder.

--- crates/vdo/src/lib.rs
+++ crates/vdo/src/lib.rs
@@ -1,9 +1,46 @@
+//! Stream buffer strategy seems to be round robin. Allocate N buffers with
+//! `vod_stream_buffer_alloc`, enqueue the buffers with,
+//! `vdo_stream_buffer_alloc`, and VDO will round robin the buffers and place
+//! frame data in them.
+//!
+
+use glib::translate::from_glib_full;
+use glib_sys::{gboolean, gpointer, GError};
 use gobject_sys::{g_object_unref, GObject};
 use std::ffi::CString;
+use std::ptr;
 use vdo_sys::*;
+
+macro_rules! try_func {
+    ($func:ident $(,)?) => {{
+        let mut error: *mut GError = ptr::null_mut();
+        let success = $func(&mut error);
+        if error.is_null() {
+            (success, None)
+        } else {
+            (success, Some(Error::VDOError(VDOError{inner: error})))
+        }
+    }};
+    ($func:ident, $($arg:expr),+ $(,)?) => {{
+        let mut error: *mut GError = ptr::null_mut();
+        let success = $func($( $arg ),+, &mut error);
+        if error.is_null() {
+            (success, None)
+        } else {
+            (success, Some(Error::VDOError(VDOError{inner: error})))
+        }
+
+    }}
+}
+
+#[derive(Debug)]
+pub struct VDOError {
+    inner: *mut GError,
+}
 
 type Result<T> = std::result::Result<T, Error>;
 pub enum Error {
+    VDOError(VDOError),
     NullPointer,
     CStringAllocation,
 }
@@ -105,20 +142,44 @@ impl StreamBuilder {
         StreamBuilder::default()
     }
 
+    pub fn with_channel(mut self, chan: u32) -> Self {
+        self.channel = chan;
+        self
+    }
+    pub fn with_format(mut self, format: VdoFormat) -> Self {
+        self.format = format;
+        self
+    }
+    pub fn with_width(mut self, width: u32) -> Self {
+        self.width = width;
+        self
+    }
+    pub fn with_height(mut self, height: u32) -> Self {
+        self.height = height;
+        self
+    }
+
     pub fn build(self) -> Result<Stream> {
         let map = Map::new()?;
         map.set_u32("channel", self.channel)?;
         map.set_u32("format", self.format as u32)?;
         map.set_u32("width", self.width)?;
         map.set_u32("height", self.height)?;
-        Ok(Stream {})
+        let (stream_raw, maybe_error) = unsafe { try_func!(vdo_stream_new, map.raw, None) };
+        Ok(Stream { raw: stream_raw })
     }
 }
 
-pub struct Stream {}
+pub struct Stream {
+    raw: *mut VdoStream,
+}
 
 impl Stream {
-    fn builder() -> StreamBuilder {
+    pub fn builder() -> StreamBuilder {
         StreamBuilder::new()
+    }
+
+    pub fn new() -> Result<Self> {
+        StreamBuilder::new().build()
     }
 }
