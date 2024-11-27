8b32af6cf4399f834e5c4e033bf06098dac5b8fd
Anders-Petter Ljungquist <anderspl@axis.com>
2024-11-27 17:05:56 +0100

    Run FFI patterns under Miri
    The new tests are included during normal test runs because to make
    sure they are correct also outside of miri; I accidentally introduced
    a concurrency bug by factoring out the code that uses a static atomic
    int and storing 0 to reset the count between runs. But outside of miri
    the tests run in parallel and this was insufficient.
    
    I have tested a variety of ways to make the code easier to read. Most
    failed under miri, but I plan to introduce the one that works in a
    later commit.

---
+++ crates/ffi_patterns/src/tests.rs
@@ -0,0 +1,218 @@
+use std::collections::HashMap;
+use std::ffi::c_void;
+use std::ptr;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::Mutex;
+use std::thread::{spawn, JoinHandle};
+
+mod sys {
+    use std::collections::HashMap;
+    use std::ffi::c_void;
+
+    type Callback = unsafe extern "C" fn(*mut c_void);
+
+    struct Subscription {
+        callback: Option<Callback>,
+        user_data: *mut c_void,
+    }
+
+    pub struct Handler {
+        next_key: i32,
+        subscriptions: HashMap<i32, Subscription>,
+    }
+
+    pub extern "C" fn handler_new() -> *mut Handler {
+        Box::into_raw(Box::new(Handler {
+            next_key: 0,
+            subscriptions: HashMap::new(),
+        }))
+    }
+
+    pub unsafe extern "C" fn handler_free(handler: *mut Handler) {
+        drop(Box::from_raw(handler));
+    }
+
+    pub unsafe extern "C" fn handler_run(handler: *mut Handler) {
+        let handler = handler.as_mut().unwrap();
+        for v in handler.subscriptions.values_mut() {
+            for _ in 0..2 {
+                if let Some(f) = v.callback {
+                    f(v.user_data);
+                }
+            }
+        }
+    }
+
+    pub unsafe extern "C" fn handler_subscribe(
+        handler: *mut Handler,
+        callback: Option<Callback>,
+        user_data: *mut c_void,
+    ) -> i32 {
+        let handler = handler.as_mut().unwrap();
+        let key = handler.next_key;
+        handler.next_key += 1;
+        handler.subscriptions.insert(
+            key,
+            Subscription {
+                callback,
+                user_data,
+            },
+        );
+        key
+    }
+}
+
+struct Deferred {
+    func: Option<Box<dyn FnOnce()>>,
+}
+
+impl Drop for Deferred {
+    fn drop(&mut self) {
+        let func = self.func.take().unwrap();
+        func();
+    }
+}
+
+impl Deferred {
+    pub fn new<F>(func: F) -> Self
+    where
+        F: FnOnce() + 'static,
+    {
+        Self {
+            func: Some(Box::new(func)),
+        }
+    }
+}
+
+struct Handler {
+    raw: *mut sys::Handler,
+    callbacks: Mutex<HashMap<Subscription, Deferred>>,
+}
+
+impl Handler {
+    fn new() -> Self {
+        Self {
+            raw: sys::handler_new(),
+            callbacks: Mutex::new(HashMap::new()),
+        }
+    }
+}
+
+impl Drop for Handler {
+    fn drop(&mut self) {
+        unsafe {
+            sys::handler_free(self.raw);
+        }
+    }
+}
+
+unsafe impl Send for Handler {}
+
+impl Handler {
+    fn spawn(&self) -> JoinHandle<()> {
+        #[derive(Debug)]
+        struct MyPtr(*mut sys::Handler);
+        unsafe impl Send for MyPtr {}
+
+        impl MyPtr {
+            fn as_ptr(&self) -> *mut sys::Handler {
+                self.0
+            }
+        }
+        let ptr = MyPtr(self.raw);
+        unsafe { spawn(move || sys::handler_run(ptr.as_ptr())) }
+    }
+    fn subscribe<F>(&self, callback: Option<F>) -> Subscription
+    where
+        F: FnMut() + Send + 'static,
+    {
+        let callback = callback.map(|c| Box::into_raw(Box::new(c)));
+        unsafe {
+            let handle = sys::handler_subscribe(
+                self.raw,
+                if callback.is_none() {
+                    None
+                } else {
+                    Some(Self::trampoline::<F>)
+                },
+                match callback {
+                    None => ptr::null_mut(),
+                    Some(callback) => callback as *mut c_void,
+                },
+            );
+            let handle = Subscription(handle);
+            if let Some(callback) = callback {
+                self.callbacks
+                    .lock()
+                    .unwrap()
+                    .insert(handle, Deferred::new(move || drop(Box::from_raw(callback))));
+            }
+            handle
+        }
+    }
+
+    fn unsubscribe(&self, handle: Subscription) {
+        self.callbacks.lock().unwrap().remove(&handle);
+    }
+
+    unsafe extern "C" fn trampoline<F>(user_data: *mut c_void)
+    where
+        F: FnMut(),
+    {
+        let callback = &mut *(user_data as *mut F);
+        callback();
+    }
+}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+struct Subscription(i32);
+
+#[test]
+fn transfer_callbacks_with_unsubscribe() {
+    let handler = Handler::new();
+
+    static SHARED_COUNT: AtomicUsize = AtomicUsize::new(0);
+    let s1 = handler.subscribe(Some(|| {
+        SHARED_COUNT.fetch_add(1, Ordering::Relaxed);
+    }));
+
+    let mut exclusive_count = 0;
+    let s2 = handler.subscribe(Some(move || {
+        exclusive_count += 1;
+        println!("{exclusive_count}")
+    }));
+
+    let s3 = handler.subscribe::<fn()>(None);
+
+    handler.spawn().join().unwrap();
+
+    handler.unsubscribe(s2);
+    handler.unsubscribe(s1);
+    handler.unsubscribe(s3);
+
+    assert_eq!(SHARED_COUNT.load(Ordering::Relaxed), 2);
+    assert_eq!(exclusive_count, 0);
+}
+
+#[test]
+fn transfer_callbacks_without_unsubscribe() {
+    let handler = Handler::new();
+
+    static SHARED_COUNT: AtomicUsize = AtomicUsize::new(0);
+    handler.subscribe(Some(|| {
+        SHARED_COUNT.fetch_add(1, Ordering::Relaxed);
+    }));
+
+    let mut exclusive_count = 0;
+    handler.subscribe(Some(move || {
+        exclusive_count += 1;
+        println!("{exclusive_count}")
+    }));
+
+    handler.subscribe::<fn()>(None);
+
+    handler.spawn().join().unwrap();
+
+    assert_eq!(SHARED_COUNT.load(Ordering::Relaxed), 2);
+    assert_eq!(exclusive_count, 0);
+}
