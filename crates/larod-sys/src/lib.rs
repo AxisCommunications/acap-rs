fa2082e86da7bbf6db94a91d978dd73b30299bed
Jonathan Green <JsGjKJzi@gmail.com>
2024-12-12 17:03:21 -0500

    Drop larod.

--- crates/larod-sys/src/lib.rs
+++
@@ -1,5 +0,0 @@
-#[cfg(not(target_arch = "x86_64"))]
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-
-#[cfg(target_arch = "x86_64")]
-include!("bindings.rs");
--- crates/larod-sys/wrapper.h
+++
@@ -1,1 +0,0 @@
-#include <larod.h>
