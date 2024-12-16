63681bbc387241d158770e298a082aec6ee855f7
Anders-Petter Ljungquist <anderspl@axis.com>
2024-12-16 10:51:36 +0100

    s: Replace semi-automated workflow with a guide
    `docs/sanitizers.md`:
    - The steps are documented instead of automated because it makes it
      easier for readers to adapt the steps if they do no longer work, or
      never worked, for their needs. One downside of this is that it
      becomes more difficult to test that the instructions are correct, but
      for now this seems like a good choice. In the future it would be
      interesting to leverage the cargo runner option to partially automate
      locating, uploading and running the executable.
    
    Change-Id: Ia97b43e2f1fe24797823c4522448987b2dbf8fa1

--- bin/run-on-device.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-set -eux
-
-AXIS_PACKAGE=$(basename ${1} | sed 's/-.*//')
-
-scp ${1} ${AXIS_DEVICE_USER}@${AXIS_DEVICE_IP}:/usr/local/packages/${AXIS_PACKAGE}/${AXIS_PACKAGE}
-ssh ${AXIS_DEVICE_USER}@${AXIS_DEVICE_IP} ${REMOTE_ENV:-} /usr/local/packages/${AXIS_PACKAGE}/${AXIS_PACKAGE}
