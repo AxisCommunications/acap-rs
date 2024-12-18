8cf8ae81cf910145b5b001af1c04fcce07aa669f
Anders-Petter Ljungquist <anderspl@axis.com>
2024-12-18 10:55:31 +0100

    f: Use `scp -p` instead of `ssh chmod +x`
    This is one less ssh call
    
    Change-Id: If2f75db4ee5028b6a413febf124be31dcf47e7b8

--- remote-test.sh
+++ remote-test.sh
@@ -4,8 +4,7 @@ set -eu
 if [ -n "${AXIS_DEVICE_IP}" ]; then
     CARGO_TEST_CAMERA=${AXIS_DEVICE_USER:-root}@${AXIS_DEVICE_IP}
     f=`basename $1`
-    scp "$1" $CARGO_TEST_CAMERA:.
-    ssh $CARGO_TEST_CAMERA "chmod +x /root/$f"
+    scp -p "$1" $CARGO_TEST_CAMERA:.
     shift
     ssh $CARGO_TEST_CAMERA "/root/$f" "$@"
 else
