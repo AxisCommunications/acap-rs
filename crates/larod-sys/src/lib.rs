c62919ce7dcff3ad364bee18f34deff1995f95f2
Jonathan Green <JsGjKJzi@gmail.com>
2025-02-14 08:25:26 -0500

    suppress name format lints

--- crates/larod-sys/src/lib.rs
+++ crates/larod-sys/src/lib.rs
@@ -1,3 +1,7 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
 #[cfg(not(target_arch = "x86_64"))]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
