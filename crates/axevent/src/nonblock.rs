22d93716f78b878fc451c70876ec4e594a13610d
Gustaf Oxenstierna <gustafo@axis.com>
2025-03-26 10:39:45 +0100

    Rename few variables

--- crates/axevent/src/nonblock.rs
+++ crates/axevent/src/nonblock.rs
@@ -17,11 +17,6 @@ use pin_project::{pin_project, pinned_drop};
 /// Users should ensure that they regularly poll this stream since it uses an internal queue
 /// to store incoming events. Currently this queue is unbounded which means that creating a
 /// subscription and then never iterating over the events will eventually fill up the memory.
-///
-/// # Examples
-///
-/// ```
-/// ```
 #[pin_project(PinnedDrop)]
 pub struct Subscription<'a> {
     handler: &'a Handler,
@@ -46,14 +41,14 @@ impl<'a> Subscription<'a> {
     ) -> Result<Self, crate::flex::Error> {
         let (tx, rx) = async_channel::unbounded::<Event>();
         let waker = Arc::new(AtomicWaker::new());
-        let inner = tx.clone();
-        let to_cb = waker.clone();
+        let axevent_tx = tx.clone();
+        let axevent_waker = waker.clone();
         let subscription = handler.subscribe(subscription_specification, move |_, evt| {
-            if let Err(e) = inner.try_send(evt) {
+            if let Err(e) = axevent_tx.try_send(evt) {
                 warn!("Unable to queue event due to {e}");
                 return;
             }
-            to_cb.wake();
+            axevent_waker.wake();
         })?;
         Ok(Self {
             handler,
