fb100293a27bd30f606f4b33ee03875727806b2c
Jonathan Green <JsGjKJzi@gmail.com>
2025-04-19 22:22:01 -0400

    add vdo-sys

---
+++ crates/vdo-sys/src/lib.rs
@@ -0,0 +1,33 @@
+//! Fairly nasty binding for VdoResolutionSet due to "Flexible Array Member"
+//! https://rust-lang.github.io/rust-bindgen/using-fam.html
+//! https://github.com/rust-lang/rust-bindgen/issues/1680#issuecomment-554296347
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+use glib_sys::*;
+use gobject_sys::*;
+
+// Information from glib types
+// https://docs.gtk.org/glib/types.html
+type gchar = core::ffi::c_char;
+type guchar = core::ffi::c_uchar;
+type gdouble = core::ffi::c_double;
+
+type gint = core::ffi::c_int;
+type guint = core::ffi::c_uint;
+type gint16 = i16;
+type guint16 = u16;
+type gint32 = i32;
+type guint32 = u32;
+type gint64 = i64;
+type guint64 = u64;
+
+type gsize = usize;
+type gssize = isize;
+
+#[cfg(not(target_arch = "x86_64"))]
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[cfg(target_arch = "x86_64")]
+include!("bindings.rs");
